a8deab0b01f404bf61b924c56da33af13a90c739
scene1019 <scene1019@gmail.com>
2016-12-26 13:30:12 +0900

    Update practice12_pyramid_setBlocks_LS.py

--- practice12_pyramid_setBlocks_LS.py
+++ practice12_pyramid_setBlocks_LS.py
@@ -9,4 +9,4 @@ size = 10
 height = size//2 + 1
 
 for i in range(height):
-    mc.setBlocks(x+i, y+i, z+i, x+size-i, y+i, z+size-i, blockType) 
+    mc.setBlocks(x-i, y+i, z-i, x+i, y+i, z+i, blockType) 
