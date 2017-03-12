b26a8f5f2bc609590de57f5ed444d83719852075
scene1019 <leeseul@leeSeului-MacBook-Pro.local>
2017-03-12 19:46:09 +0900

    update 17.03.12.

--- practice23_Laputa.py
+++ practice23_Laputa.py
@@ -7,28 +7,250 @@ pos = mc.player.getTilePos()
 x = pos.x
 y = pos.y
 z = pos.z
-blockType = 41
 
-
+# 첫 번째 블록은 금 블록, blockType = 41
 x1 = random.randint(x-10, x+10)
 y1 = y + 10
 z1 = random.randint(z-10, z+10)
 mc.player.setTilePos(x1, y1, z1)
-mc.setBlock(x1, y1-1, z1, blockType)
+mc.setBlock(x1, y1-1, z1, 41)
 
+
+# 두 번째 블록은 다이아몬드, blockType = 57
 x2 = random.randint(x-10, x+10)
 y2 = y + 10
 z2 = random.randint(z-10, z+10)
 mc.player.setTilePos(x2, y2, z2)
-mc.setBlock(x2, y2-1, z2, blockType)
+mc.setBlock(x2, y2-1, z2, 57)
 
+# 세 번째 블록은 에메랄드, blockType = 133
 x3 = random.randint(x-10, x+10)
 y3 = y + 10
 z3 = random.randint(z-10, z+10)
 mc.player.setTilePos(x3, y3, z3)
-mc.setBlock(x3, y3-1, z3, blockType)
+mc.setBlock(x3, y3-1, z3, 133)
 
 
+i = 0
+while 0 == mc.getBlock(x1+1, y1-1-i, z1+1+i):
+    mc.setBlock(x1+1, y1-1-i, z1+1+i, 41)
+    i += 1
+
+i = 0
+while 0 == mc.getBlock(x2+1, y2-1-i, z2+1+i):
+    mc.setBlock(x2+1, y2-1-i, z2+1+i, 57)
+    i += 1
+
+i = 0
+while 0 == mc.getBlock(x3+1, y3-1-i, z3+1+i):
+    mc.setBlock(x3+1, y3-1-i, z3+1+i, 133)
+    i += 1
+
+mc.postToChat("1: (%d, %d, %d)  2: (%d, %d, %d)  3: (%d, %d, %d)" %(x1, y1, z1, x2, y2, z2, x3, y3, z3))
+
+# 1 to 2
+if (x2 - x1 > 0) and (z2 - z1 > 0):
+    while x2 - x1 != 0:
+        mc.setBlock(x1+1, y1-1, z1, 17) # blockType = 17 = 가문비나무
+        x1 += 1
+    while z2 - z1 != 0:
+        mc.setBlock(x1, y1-1, z1, 1) # blockType = 1 = 돌
+        z1 += 1
+
+elif (x2 - x1 > 0) and (z2 - z1 < 0):
+    while x2 - x1 != 0:
+        mc.setBlock(x1+1, y1-1, z1, 17)
+        x1 += 1
+    while z2 - z1 != 0:
+        mc.setBlock(x1, y1-1, z1, 1)
+        z1 -= 1
+
+elif (x2 - x1 < 0) and (z2 - z1 > 0):
+    while x2 - x1 != 0:
+        mc.setBlock(x1-1, y1-1, z1, 17)
+        x1 -= 1
+    while z2 - z1 != 0:
+        mc.setBlock(x1, y1-1, z1, 1)
+        z1 += 1
+
+else:
+    while x2 - x1 != 0:
+        mc.setBlock(x1-1, y1-1, z1, 17)
+        x1 -= 1
+    while z2 - z1 != 0:
+        mc.setBlock(x1, y1-1, z1, 1)
+        z1 -= 1
+
+'''
+# 1 to 3
+if (x3 - x1 > 0) and (z3 - z1 > 0):
+    while x3 - x1 != 0:
+        mc.setBlock(x1+1, y1-1, z1, 17)
+        x1 += 1
+    while z3 - z1 != 0:
+        mc.setBlock(x1, y1-1, z1, 1)
+        z1 += 1
+
+elif (x3 - x1 > 0) and (z3 - z1 < 0):
+    while x3 - x1 != 0:
+        mc.setBlock(x1+1, y1-1, z1, 17)
+        x1 += 1
+    while z3 - z1 != 0:
+        mc.setBlock(x1, y1-1, z1, 1)
+        z1 -= 1
+
+elif (x3 - x1 < 0) and (z3 - z1 > 0):
+    while x3 - x1 != 0:
+        mc.setBlock(x1-1, y1-1, z1, 17)
+        x1 -= 1
+    while z3 - z1 != 0:
+        mc.setBlock(x1, y1-1, z1, 1)
+        z1 += 1
+
+else:
+    while x3 - x1 != 0:
+        mc.setBlock(x1-1, y1-1, z1, 17)
+        x1 -= 1
+    while z3 - z1 != 0:
+        mc.setBlock(x1, y1-1, z1, 1)
+        z1 -= 1
+'''
+
+'''
+# 2 to 1
+if (x1 - x2 > 0) and (z1 - z2 > 0):
+    while x1 - x2 != 0:
+        mc.setBlock(x2+1, y2-1, z2, 17)
+        x2 += 1
+    while z1 - z2 != 0:
+        mc.setBlock(x2, y2-1, z2, 1)
+        z2 += 1
+
+elif (x1 - x2 > 0) and (z1 - z2 < 0):
+    while x1 - x2 != 0:
+        mc.setBlock(x2+1, y2-1, z2, 17)
+        x2 += 1
+    while z1 - z2 != 0:
+        mc.setBlock(x2, y2-1, z2, 1)
+        z2 -= 1
+
+elif (x1 - x2 < 0) and (z1 - z2 > 0):
+    while x1 - x2 != 0:
+        mc.setBlock(x2-1, y2-1, z2, 17)
+        x2 -= 1
+    while z1 - z2 != 0:
+        mc.setBlock(x2, y2-1, z2, 1)
+        z2 += 1
+
+else:
+    while x1 - x2 != 0:
+        mc.setBlock(x2-1, y2-1, z2, 17)
+        x2 -= 1
+    while z1 - z2 != 0:
+        mc.setBlock(x2, y2-1, z2, 1)
+        z2 -= 1
+'''
+# 2 to 3
+if (x3 - x2 > 0) and (z3 - z2 > 0):
+    while x3 - x2 != 0:
+        mc.setBlock(x2+1, y2-1, z2, 17)
+        x2 += 1
+    while z3 - z2 != 0:
+        mc.setBlock(x2, y2-1, z2, 1)
+        z2 += 1
+
+elif (x3 - x2 > 0) and (z3 - z2 < 0):
+    while x3 - x2 != 0:
+        mc.setBlock(x2+1, y2-1, z2, 17)
+        x2 += 1
+    while z3 - z2 != 0:
+        mc.setBlock(x2, y2-1, z2, 1)
+        z2 -= 1
+
+elif (x3 - x2 < 0) and (z3 - z2 > 0):
+    while x3 - x2 != 0:
+        mc.setBlock(x2-1, y2-1, z2, 17)
+        x2 -= 1
+    while z3 - z2 != 0:
+        mc.setBlock(x2, y2-1, z2, 1)
+        z2 += 1
+
+else:
+    while x3 - x2 != 0:
+        mc.setBlock(x2-1, y2-1, z2, 17)
+        x2 -= 1
+    while z3 - z2 != 0:
+        mc.setBlock(x2, y2-1, z2, 1)
+        z2 -= 1
+
+# 3 to 1
+if (x1 - x3 > 0) and (z1 - z3 > 0):
+    while x1 - x3 != 0:
+        mc.setBlock(x3+1, y3-1, z3, 17)
+        x3 += 1
+    while z1 - z3 != 0:
+        mc.setBlock(x3, y3-1, z3, 1)
+        z3 += 1
+
+elif (x1 - x3 > 0) and (z1 - z3 < 0):
+    while x1 - x3 != 0:
+        mc.setBlock(x3+1, y3-1, z3, 17)
+        x3 += 1
+    while z1 - z3 != 0:
+        mc.setBlock(x3, y3-1, z3, 1)
+        z3 -= 1
+
+elif (x1 - x3 < 0) and (z1 - z3 > 0):
+    while x1 - x3 != 0:
+        mc.setBlock(x3-1, y3-1, z3, 17)
+        x3 -= 1
+    while z1 - z3 != 0:
+        mc.setBlock(x3, y3-1, z3, 1)
+        z3 += 1
+
+else:
+    while x1 - x3 != 0:
+        mc.setBlock(x3-1, y3-1, z3, 17)
+        x3 -= 1
+    while z1 - z3 != 0:
+        mc.setBlock(x3, y3-1, z3, 1)
+        z3 -= 1
+
+'''
+# 3 to 2
+if (x2 - x3 > 0) and (z2 - z3 > 0):
+    while x2 - x3 != 0:
+        mc.setBlock(x3+1, y3-1, z3, 17)
+        x3 += 1
+    while z2 - z3 != 0:
+        mc.setBlock(x3, y3-1, z3, 1)
+        z3 += 1
+
+elif (x2 - x3 > 0) and (z2 - z3 < 0):
+    while x2 - x3 != 0:
+        mc.setBlock(x3+1, y3-1, z3, 17)
+        x3 += 1
+    while z2 - z3 != 0:
+        mc.setBlock(x3, y3-1, z3, 1)
+        z3 -= 1
+
+elif (x2 - x3 < 0) and (z2 - z3 > 0):
+    while x2 - x3 != 0:
+        mc.setBlock(x3-1, y3-1, z3, 17)
+        x3 -= 1
+    while z2 - z3 != 0:
+        mc.setBlock(x3, y3-1, z3, 1)
+        z3 += 1
+
+else:
+    while x2 - x3 != 0:
+        mc.setBlock(x3-1, y3-1, z3, 17)
+        x3 -= 1
+    while z2 - z3 != 0:
+        mc.setBlock(x3, y3-1, z3, 1)
+        z3 -= 1
+'''
+'''
 roadx21 = x2 - x1
 roadz21 = z2 - z1
 reviseZ21 = abs(roadz21) - 1
@@ -87,7 +309,7 @@ else:
             j += 1
             mc.setBlock(x2+roadx32, y2-1, z2-j, 1)
 
-
+'''
 '''
 roadx32 = x3 - x2
 roadz32 = z3 - z2
